300b3c9ea63309ab407da0acb1de8165de68bc13
Evan Reilly <ereilly1@uvm.edu>
2017-04-09 15:54:24 -0400

    Implemented Point2d, tested, functioning

--- swarm.js
+++ swarm.js
@@ -16,28 +16,59 @@ canvas.width = width;
 canvas.height = height;
 ctx = canvas.getContext('2d');
 //Objects
+
+
+function Point2d(x1,y1){
+  this.x1 = x1;
+  this.y1 = y1;
+  this.normalize = function(){
+    v = Math.sqrt(Math.pow(this.x1,2)+Math.pow(this.y1,2));
+    this.x1 = this.x1/v;
+    this.y1 = this.y1/v;
+  }
+  this.magnitude = function(){
+    v = Math.sqrt(Math.pow(this.x1,2)+Math.pow(this.y1,2));
+    return v;
+  }
+  this.add = function(point2d){
+    this.x1 = this.x1+point2d.getX1();
+    this.y1 = this.y1+point2d.getY1();
+  }
+  this.multiply = function(factor){
+    this.x1 = this.x1*factor;
+    this.y1 = this.y1 *factor;
+  }
+  this.setX1 = function(x1){this.x1 = x1;}
+  this.setY1 = function(y1){this.y1 = y1;}
+  this.getX1 = function(){return this.x1;}
+  this.getY1 = function(){return this.y1;}
+
+}
+
 function Line(x1, y1, x2, y2) {
     this.x1 = x1;
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
+    this.strokeStyle = "rgba(156,217,249,0.5)";
     this.draw = function() {
         ctx.beginPath();
         ctx.moveTo(x1, y1);
         ctx.lineTo(x2, y2);
         //ctx.strokeStyle = "#FF0000";
-        ctx.strokeStyle = "rgba(156,217,249,0.5)";
+        ctx.strokeStyle = this.strokeStyle;
         ctx.stroke();
-
     }
+    this.setX1 = function(x1){this.x1 = x1;}
+    this.setY1 = function(y1){this.y1 = y1;}
+    this.setX2 = function(x2){this.x2 = x2;}
+    this.setY2 = function(y2){this.y2 = y2;}
+    this.setColor = function(color){this.strokeStyle = color;}
+
 }
 
 //bees = [];
-var randomX1 = center.x - (width / 40) + Math.random() * width / 20
-var randomY1 = center.y - (width / 40) + Math.random() * width / 20
-var randomX2 = center.x - (width / 40) + Math.random() * width / 20
-var randomY2 = center.y - (width / 40) + Math.random() * width / 20
-var line = new Line(randomX1, randomY1, randomX2, randomY2);
+
 
 var bee = {
 
@@ -46,15 +77,29 @@ var bee = {
     x2: randomX2,
     y2: randomY2
 }
-initAnimation();
+
 
 function initAnimation(){
   animate();
 }
 
+var randomX1 = center.x - (width / 40) + Math.random() * width / 20
+var randomY1 = center.y - (width / 40) + Math.random() * width / 20
+var randomX2 = center.x - (width / 40) + Math.random() * width / 20
+var randomY2 = center.y - (width / 40) + Math.random() * width / 20
+var line = new Line(randomX1, randomY1, randomX2, randomY2);
 
-
-
+line.setColor("rgba(156,200,249,0.5)")
+var point1 = new Point2d(0,0);
+var point2 = new Point2d(1,2);
+console.log(point1['x1']);
+point1.add(point2);
+console.log(point1['x1']);
+console.log(point1['y1']);
+point1.normalize();
+console.log(point1['x1']);
+console.log(point1['y1']);
+initAnimation();
 function animate(){
   line.draw();
   requestAnimationFrame(animate);
